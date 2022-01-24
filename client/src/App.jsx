f95f9e873e33ee07c6bc8a5c2fba53cf656d2c68
indjar <indre.bacvinkaite@gmail.com>
2022-01-24 23:27:51 +0200

    removed not usable css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
 import {Register} from "./pages/Register";
 import {AuthProvider} from "./components/AuthProvider";
