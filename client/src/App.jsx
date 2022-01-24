42b7a30ae0daf18279df53f6f2ed835eec793e86
indjar <indre.bacvinkaite@gmail.com>
2022-01-24 23:26:27 +0200

    added register page to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
-
 import './App.css';
+import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
+import {Register} from "./pages/Register";
+import {AuthProvider} from "./components/AuthProvider";
+import {RequireAuth} from "./components/RequireAuth";
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+     <AuthProvider>
+
+       <Routes>
+         <Route path='/register' element={<Register/>}></Route>
+       </Routes>
+
+     </AuthProvider>
+  </Router>
   );
 }
 
