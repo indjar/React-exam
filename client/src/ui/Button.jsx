65ad4fdc12056d5c9e594ff5b36ac95ae66af592
indjar <indre.bacvinkaite@gmail.com>
2022-01-25 15:13:09 +0200

    button font changed

--- client/src/ui/Button.jsx
+++ client/src/ui/Button.jsx
@@ -8,7 +8,7 @@ export const Button=styled(button)`
 background: ${props => props.login ? "#1d3972" : "white"};
 color: ${props => props.register ? "white" : "#1d3972"};
 max-width:150px;
-font-size: 24px;
+font-size: 20px;
 margin: 14px;
 padding: 0.25em 1em;
 border: 2px solid #1d3972;
